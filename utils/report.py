c7a16cafae304386066ffa5bd3c4bd6b5c989382
Hanbeomrae <your@email.com>
2025-06-30 17:58:53 +0900

    style: 한글 변경

--- utils/report.py
+++ utils/report.py
@@ -7,9 +7,9 @@ REPORT_FILE = "trade_report.json"
 def init_report():
     if not os.path.exists(REPORT_FILE):
         data = {
-            "tp1_count": 0,
-            "tp2_count": 0,
-            "stop_loss_count": 0
+            "1차 익절 횟수 ": 0,
+            "2차 익절 횟수": 0,
+            "손절 횟수": 0
         }
         with open(REPORT_FILE, "w") as f:
             json.dump(data, f)
